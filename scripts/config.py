7f6c53b1a1e00e1bf0381a4fc072a6ae7f3278e9
kirillgarbar <st087492@student.spbu.ru>
2022-12-26 01:19:02 +0300

    Matrices listed in different file

--- scripts/config.py
+++ scripts/config.py
@@ -6,6 +6,7 @@ from dataclasses import dataclass
 ROOT = pathlib.Path(__file__).parent.parent
 DATASET = ROOT / "dataset"
 DEPS = ROOT / "deps"
+MATRICES = ROOT / "scripts" / "matrices.txt"
 
 SYSTEM = {'Darwin': 'macos', 'Linux': 'linux', 'Windows': 'windows'}[platform.system()]
 EXECUTABLE_EXT = {'macos': '', 'windows': '.exe', 'linux': ''}[SYSTEM]
@@ -15,19 +16,7 @@ DATASET_SUFFIX = ""
 DEFAULT_NUM_ITERATIONS = 10
 DEFAULT_SOURCE_VERTEX = 0
 
-GRAPHS_NAMES = [
-    'wing',
-    'coAuthorsCiteseer',
-    'coPapersDBLP',
-    'hollywood-2009',
-    'roadNet-CA',
-    'com-Orkut',
-    'cit-Patents',
-    'rgg_n_2_22_s0',
-    'soc-LiveJournal',
-    'indochina-2004',
-    'rgg_n_2_23_s0'
-]
+GRAPHS_NAMES = open(MATRICES, 'r').readlines()
 
 
 @dataclass
@@ -52,21 +41,11 @@ class Graph:
 
 GRAPHS_DATA = {name: Graph(name) for name in GRAPHS_NAMES}
 
-GRAPHS_BFS = [
-    GRAPHS_DATA['wing']
-]
+GRAPHS_BFS = [ GRAPHS_DATA[name] for name in GRAPHS_DATA ]
 
-GRAPHS_SSSP = []
+GRAPHS_SSSP = [ GRAPHS_DATA[name] for name in GRAPHS_DATA ]
 
-GRAPHS_TC = [
-    GRAPHS_DATA['coAuthorsCiteseer'],
-    GRAPHS_DATA['coPapersDBLP'],
-    GRAPHS_DATA['roadNet-CA'],
-    GRAPHS_DATA['com-Orkut'],
-    GRAPHS_DATA['cit-Patents'],
-    GRAPHS_DATA['soc-LiveJournal'],
-    GRAPHS_DATA['rgg_n_2_22_s0']
-]
+GRAPHS_TC = [ GRAPHS_DATA[name] for name in GRAPHS_DATA ]
 
 ALGORITHMS = ["bfs", "sssp", "tc"]
 GRAPHS = {"bfs": GRAPHS_BFS, "sssp": GRAPHS_SSSP, "tc": GRAPHS_TC}
