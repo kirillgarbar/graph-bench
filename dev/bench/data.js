2ee76bd7ca52d57eb575b7a5e08000a95dfeb825
github-action-benchmark <github@users.noreply.github.com>
2023-01-28 05:08:56 +0300

    add BFS (benchmarkdotnet) benchmark result for 70324383135568e02525be98209635ba397dbb5a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1674870746209,
+  "lastUpdate": 1674871735778,
   "repoUrl": "https://github.com/kirillgarbar/graph-bench",
   "entries": {
     "BFS": [
@@ -30,6 +30,36 @@ window.BENCHMARK_DATA = {
             "value": 239382440.875,
             "unit": "ns",
             "range": "± 5659628.858894759"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "st087492@student.spbu.ru",
+            "name": "kirillgarbar",
+            "username": "kirillgarbar"
+          },
+          "committer": {
+            "email": "st087492@student.spbu.ru",
+            "name": "kirillgarbar",
+            "username": "kirillgarbar"
+          },
+          "distinct": true,
+          "id": "70324383135568e02525be98209635ba397dbb5a",
+          "message": "Dotnet 6",
+          "timestamp": "2023-01-28T04:51:33+03:00",
+          "tree_id": "ade0870ba63039b9b084d02d743e7168a8bf2859",
+          "url": "https://github.com/kirillgarbar/graph-bench/commit/70324383135568e02525be98209635ba397dbb5a"
+        },
+        "date": 1674871734945,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "GraphBLAS.FSharp.Benchmarks.BFSBenchmarksWithoutDataTransfer.Benchmark(OclContextInfo: ({ ClContext = GeForce RTX 2070\n  Queue = Microsoft.FSharp.Control.FSharpMailboxProcessor`1[Brahma.FSharp.Msg] }, 32), InputMatrixReader: wing.mtx)",
+            "value": 253010296.4,
+            "unit": "ns",
+            "range": "± 17891106.711579826"
           }
         ]
       }
