1d06675c3ecd6f20e49e57aa8414201690c5eea4
kirillgarbar <st087492@student.spbu.ru>
2023-01-30 05:22:09 +0300

    Configs for GBS

--- scripts/benchmark_graphblas-sharp.py
+++ scripts/benchmark_graphblas-sharp.py
@@ -8,14 +8,9 @@ dest_directory = config.DEPS / "graphblas-sharp" / "benchmarks" / "GraphBLAS-sha
 subprocess.call(f'cp {config_directory / "config_graphblas-sharp.txt"} {dest_directory / "Configs" / "Context.txt"}', shell=True)
 subprocess.call(f'cp -a {config.DATASET / "."} {dest_directory / "Datasets"}')
 
-matrices = [matrix + ".mtx" for matrix in config.GRAPHS_NAMES]
 targets = [line.strip() for line in open(config_directory / "graphblas-sharp_targets.txt", 'r').readlines()]
 
 for target in targets:
-	#Writing config for each target
-	with open(f'{dest_directory / "Configs" / "{target}.txt"}', 'w') as file:
-		file.writelines(m + "\n" for m in matrices)
-
 	#Executing benchmarks
 	project_directory = config.DEPS / "graphblas-sharp" / "benchmarks" / "GraphBLAS-sharp.Benchmarks"
 	binaries = project_directory / "bin" / "Release" / "net7.0"
