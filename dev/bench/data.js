68a8b1a7fa517bb54b4b1d2bbdabcaaa941b3a02
github-action-benchmark <github@users.noreply.github.com>
2023-02-05 06:29:56 +0300

    add BFS (benchmarkdotnet) benchmark result for c379bdcdd19e08542e358dea54ae692292c4f71b

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1675567267913,
+  "lastUpdate": 1675567796052,
   "repoUrl": "https://github.com/kirillgarbar/graph-bench",
   "entries": {
     "BFS": [
@@ -234,6 +234,42 @@ window.BENCHMARK_DATA = {
             "value": 235094759.1,
             "unit": "ns",
             "range": "± 10418094.63371455"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "st087492@student.spbu.ru",
+            "name": "kirillgarbar",
+            "username": "kirillgarbar"
+          },
+          "committer": {
+            "email": "st087492@student.spbu.ru",
+            "name": "kirillgarbar",
+            "username": "kirillgarbar"
+          },
+          "distinct": true,
+          "id": "c379bdcdd19e08542e358dea54ae692292c4f71b",
+          "message": "Pushing correct file",
+          "timestamp": "2023-02-05T06:19:41+03:00",
+          "tree_id": "389800c282a55ef3ffc5cfda3fcc1d4d200e9b49",
+          "url": "https://github.com/kirillgarbar/graph-bench/commit/c379bdcdd19e08542e358dea54ae692292c4f71b"
+        },
+        "date": 1675567795295,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "coAuthorsCiteseer.mtx",
+            "value": 678405172.5,
+            "unit": "ns",
+            "range": "± 21503738.962657943"
+          },
+          {
+            "name": "wing.mtx",
+            "value": 237147457.6,
+            "unit": "ns",
+            "range": "± 8002940.854135756"
           }
         ]
       }
